686841e42fb9940007ff9b9840d639d7c9216cf3
Steve Raskin <steveraskin@gmail.com>
2019-04-15 11:11:45 -0400

    removed unused api component

--- src/api/googleMaps.js
+++ src/api/googleMaps.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-
-
-const KEY = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
-
-
-
-export default axios.create({
-	baseURL: "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap",
-});
+// import axios from 'axios';
+//
+//
+// const KEY = "";
+//
+//
+//
+// export default axios.create({
+// 	baseURL: "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap",
+// });
